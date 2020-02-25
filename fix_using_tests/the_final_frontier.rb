34be718a8767240fd89df1d13191af77ff8d5711
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-25 04:25:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,7 +12,9 @@ def crew
 end
 
 def greet_crew(crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  crew_greeting = []
+  crew.each {|crew_member| crew_greeting << "Hello #{crew_member}."}
+  crew_greeting
 end
 
 
